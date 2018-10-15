ff3013a733adcde71611cbe72b0d34ab7e65e313
D7701 <D7701@DESKTOP-ODBNK9S>
2018-10-15 14:11:36 +0900

    JDBC 연습

--- 201412409-exSpringOJDBC/src/main/java/kr/ac/dit/persistence/StudentDAOImpl.java
+++ 201412409-exSpringOJDBC/src/main/java/kr/ac/dit/persistence/StudentDAOImpl.java
@@ -1,39 +1,30 @@
 package kr.ac.dit.persistence;
 import java.sql.*;
 import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import kr.ac.dit.domain.StudentVO;
 @Repository
 public class StudentDAOImpl implements StudentDAO {
+	@Autowired
+		JdbcTemplate jdbcTemplate;
 	@Override
 	public void insertStudent(StudentVO studentVO) throws Exception {
-		String no = studentVO.getNo();
-		String name = studentVO.getName();
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "201412409", "201412409");
-		PreparedStatement pstmt = conn.prepareStatement("insert into student values(?,?)");
-		pstmt.setString(1, no);
-		pstmt.setString(2, name);
-		pstmt.executeUpdate();
-		pstmt.close();
-		conn.close();
-	}
-	@Override
-	public List<StudentVO> selectStudentList() throws Exception {
-		List<StudentVO> items = new ArrayList<StudentVO>();
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "201412409", "201412409");
-		PreparedStatement pstmt = conn.prepareStatement("select * from student");
-		ResultSet rs = pstmt.executeQuery();
-		while(rs.next()) {
-			StudentVO item = new StudentVO();
-			item.setNo(rs.getString("no"));
-			item.setName(rs.getString("name"));
-			items.add(item);
-		}
-		pstmt.close();
-		rs.close();
-		conn.close();
-		return items;
-	}
+		  jdbcTemplate.update("insert into student values(?,?)", studentVO.getNo(), studentVO.getName());
+	 }
+	@SuppressWarnings("rawtypes")
+	 @Override
+	  public List<StudentVO> selectStudentList() throws Exception {
+	  List<StudentVO> items = new ArrayList<StudentVO>();
+	    List<Map<String, Object>> rows = jdbcTemplate.queryForList("select * from student"); 
+	    for (Map row : rows) { 
+	      StudentVO item = new StudentVO(); 
+	      item.setNo((String) row.get("no")); 
+	      item.setName((String) row.get("name")); 
+	      items.add(item); 
+	    } 
+	    return items;
+	 }
 }
